214551ce73cc4040d511e0056303830014ccd5e0
gorakh999 <gorakhgupta999@gmail.com>
2021-05-31 11:31:27 +0530

    Added requirements.txt file

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-try:
-    github_profile = input("Enter Github User Profile : ")
 
+def github_photo():
+    github_profile = input("Enter Github User Profile : ")
     github_url = "https://github.com/" + github_profile
     response = requests.get(github_url)
-    # print(response.content)
 
     soup = bs(response.content, 'html.parser')
     profile_image = soup.find('img', {'alt' : '@' + github_profile})['src']
-    print(profile_image)
-except Exception as e:
-    print("Something Error Occured....")
+    return profile_image
 
-
+if __name__ == "__main__":
+    try:
+        result = github_photo()
+        print(result)
+    
+    except Exception as e:
+        print("Something Error Occured....")
